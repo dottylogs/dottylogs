9a9c35d5dfb69428b6f96b957edfd4ae77aa1936
Stephen Brown <steve@evolvedlight.co.uk>
2021-03-26 22:04:46 +0100

    Remove link to about screen

--- DottyLogs.Ui/client-app/src/router/index.ts
+++ DottyLogs.Ui/client-app/src/router/index.ts
@@ -15,14 +15,6 @@ const routes: Array<RouteRecordRaw> = [
         path: '/trace/:traceIdentifier',
         name: 'details',
         component: () => import(/* webpackChunkName: "details" */ '../views/Details.vue')
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
 ]
 const router = createRouter({
